1ee4fee7a18a2a18c73967311d3ae378003f3d86
Carlos Eduardo Vieira <carlosportalsocial@gmail.com>
2023-01-29 13:10:26 -0300

    criadas as migrations com foreign keys

---
+++ database/migrations/2023_01_29_154823_create_produtos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('produtos', function (Blueprint $table) {
+            $table->id();
+            $table->timestamps();
+            $table->unsignedBigInteger('fornecedor_id');
+            $table->string('titulo', 30);
+            $table->string('imagem', 100);
+            $table->string('descricao', 200);
+            $table->float('preco', 8,2);
+
+            // foreign key
+            $table->foreign('fornecedor_id')->references('id')->on('fornecedores');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('produtos');
+    }
+};
