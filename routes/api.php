c590803651ff11e1ea0510ecc6d60744061b270e
Carlos Eduardo Vieira <carlosportalsocial@gmail.com>
2023-02-02 19:35:14 -0300

    criadas a rotas para fornecedor

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ClienteController;
+use App\Http\Controllers\FornecedorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -26,4 +27,11 @@ Route::prefix('v1')->group(function () {
     Route::put('cliente/{id}', [ClienteController::class, 'update']);
     Route::patch('cliente/{id}', [ClienteController::class, 'update']);
     Route::delete('cliente/{id}', [ClienteController::class, 'destroy']);
+
+    Route::get('fornecedor', [FornecedorController::class, 'index']);
+    Route::post('fornecedor', [FornecedorController::class, 'store']);
+    Route::get('fornecedor/{id}', [FornecedorController::class, 'show']);
+    Route::put('fornecedor/{id}', [FornecedorController::class, 'update']);
+    Route::patch('fornecedor/{id}', [FornecedorController::class, 'update']);
+    Route::delete('fornecedor/{id}', [FornecedorController::class, 'destroy']);
 });
