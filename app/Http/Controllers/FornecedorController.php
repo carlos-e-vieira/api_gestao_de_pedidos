bd97e3d720b72da8bc7b348244f8564263d9c1b1
Carlos Eduardo Vieira <carlosportalsocial@gmail.com>
2023-02-05 11:41:50 -0300

    criado metodo index do controller fornecedor

--- app/Http/Controllers/FornecedorController.php
+++ app/Http/Controllers/FornecedorController.php
@@ -3,20 +3,41 @@
 namespace App\Http\Controllers;
 
 use App\Models\Fornecedor;
+use App\Repositories\FornecedorRepository;
 use Illuminate\Http\Request;
 
 class FornecedorController extends Controller
 {
     private Fornecedor $fornecedor;
+    private FornecedorRepository $fornecedorRepository;
 
     public function __construct(Fornecedor $fornecedor)
     {
         $this->fornecedor = $fornecedor;
+        $this->fornecedorRepository = new FornecedorRepository($this->fornecedor);
     }
 
     public function index(Request $request)
     {
+        // condição de busca por atributos do produto
+        if ($request->has('atributos_produtos')) {
+            $atributos_produtos = 'produtos:id'. $request->atributos_produtos;
+            $this->fornecedorRepository->selectAtributosRelacionados($atributos_produtos);
+        } else {
+            $this->fornecedorRepository->selectAtributosRelacionados('produtos');
+        }
 
+        // condição de busca com filtro
+        if ($request->has('filtro')) {
+            $this->fornecedorRepository->selectFiltros($request->filtro);
+        }
+
+        // condição de busca por atributos do forncecedor
+        if ($request->has('atributos')) {
+            $this->fornecedorRepository->selectAtributos($request->atributos);
+        }
+
+        return response()->json($this->fornecedorRepository->getResultado(), 200);
     }
 
     public function store(Request $request)
